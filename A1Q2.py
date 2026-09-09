f1542a02c42d1bd031097fdc98d57f0fd06ed79a
Nayan Godhwani <godhwaninayan12@gmail.com>
2026-09-09 10:50:29 -0700

    Implemented disk movement recursive function

--- A1Q2.py
+++ A1Q2.py
@@ -1,30 +1,41 @@
 import sys
 
+def disk_moves(disks, start, position, pegs, counter_clockwise, clockwise):
+    if disks == 0:
+        return 0
+    if disks % 2:
+        destination = counter_clockwise[start]
+    else:
+        destination = clockwise[start]
+    for peg in pegs:
+        if peg != start and peg != destination:
+            spare = peg
+            break
+    if position[disks] == start:
+        return disk_moves(disks - 1, start, position, pegs, counter_clockwise, clockwise)
+
+    if position[disks] == destination:
+        rest = disk_moves(disks - 1, spare, position, pegs, counter_clockwise, clockwise)
+        if rest == -1:
+            return -1
+        return (1 << (disks - 1)) + rest
+    return -1
+
 def tower_hanoi(n, state):
     pegs = ['A', 'B', 'C']
-
-    counter_clockwise = {
-        'A': 'C',
-        'C': 'B',
-        'B': 'A'
-    }
-
-    clockwise = {
-        'A': 'B',
-        'B': 'C',
-        'C': 'A'
-    }
-
+    counter_clockwise = {'A': 'C', 'C': 'B','B': 'A'}
+    clockwise = {'A': 'B','B': 'C','C': 'A'}
     position = {}
-
-    parts = state.split(',')
-
     for i in range(3):
-        disks = parts[i].split()
+        for disk in state[i]:
+            position[disk] = pegs[i]
+    if len(set(position.values())) == 1:
+        return position[1] + ' 0'
+    for start in pegs:
+        count = disk_moves(n, start, position, pegs, counter_clockwise, clockwise)
+        if count != -1 and count <= (1 << n) - 2:
+            return start + ' ' + str(count)
 
-        for disk in disks:
-            position[int(disk)] = pegs[i]
-    
     return 'impossible'
 
 num_case = int(sys.stdin.readline())
